d53e53acfb47ba5e868849c4e3d5abb8232d534f
Shena0110 <esshena20@gmail.com>
2023-10-20 01:31:40 +0800

    Update

--- config/database.js
+++ config/database.js
@@ -1,25 +1,29 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 async function connectDatabase() {
   const uri = "mongodb+srv://admin_0110:user0110@cluster0.tayhvzt.mongodb.net/?retryWrites=true&w=majority";
 
   const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
 
   try {
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log('Connected to MongoDB Atlas');
+
+    // Access your database and perform operations here
+    const database = client.db('<databaseName>'); // Replace '<databaseName>' with your actual database name
+
+    // Example: List collections in the database
+    const collections = await database.listCollections().toArray();
+    console.log('Collections in the database:', collections);
+
   } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
+    console.error('Error connecting to MongoDB:', err);
   } finally {
     await client.close();
   }
 }
 
-export { connectDatabase }; // Export the connectDatabase function
+export { connectDatabase };
