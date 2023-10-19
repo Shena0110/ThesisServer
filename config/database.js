5cd12787b5fdc3a8382692d8ee345ffad1febc45
Shena0110 <esshena20@gmail.com>
2023-10-20 01:06:12 +0800

    Database Connection

--- config/database.js
+++ config/database.js
@@ -1,11 +1,25 @@
-import mongoose from "mongoose";
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
-export const connectDatabase = async () => {
-    try {
-        const { connection } = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected: ${connection.host}`);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
+async function connectDatabase() {
+  const uri = "mongodb+srv://admin_0110:user0110@cluster0.tayhvzt.mongodb.net/?retryWrites=true&w=majority";
+
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
     }
+  });
+
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  } finally {
+    await client.close();
+  }
 }
+
+export { connectDatabase }; // Export the connectDatabase function
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { app } from './app.js';
 import dotenv from 'dotenv'; // Remove the curly braces
-import { connectDatabase } from './config/database.js';
+import {connectDatabase} from './config/database.js';
 
 dotenv.config({
     path: "./config/config.env",
