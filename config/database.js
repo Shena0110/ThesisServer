c7838c96cd1a28bfef4abeb30bcf5053a07e9031
Shena0110 <esshena20@gmail.com>
2023-10-20 04:13:04 +0800

    Updated

--- config/database.js
+++ config/database.js
@@ -1,19 +1,16 @@
-import { MongoClient } from 'mongodb';
+import {MongoClient} from 'mongodb';
 
 async function connectDatabase() {
-  const uri = "mongodb+srv://admin_0110:user0110@cluster0.tayhvzt.mongodb.net/?retryWrites=true&w=majority";
+  const uri = "mongodb+srv://admin_0110:user0110@cluster0.tayhvzt.mongodb.net/zamboDB?retryWrites=true&w=majority";
 
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  const client = new MongoClient(uri);
 
   try {
     await client.connect();
     console.log('Connected to MongoDB Atlas');
 
     // Access your database and perform operations here
-    const database = client.db('<databaseName>'); // Replace '<databaseName>' with your actual database name
+    const database = client.db('zamboDB'); // Replace '<databaseName>' with your actual database name
 
     // Example: List collections in the database
     const collections = await database.listCollections().toArray();
